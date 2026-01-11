caf59c27b3b1fd8724abac1496689fbf6e1e35f3
RoyalGr4pe <njames.programming@gmail.com>
2026-01-11 18:02:09 +0000

    update nextjs version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en">
-            <body className={`antialiased w-full`}>
+            <body className={`antialiased w-full translate-y-3`}>
                 {children}
 
                 <Footer />
